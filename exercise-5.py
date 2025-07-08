53ce0618522c47d901e70a9d6883db3a28cf309e
Kheang <2024374lim@aupp.edu.kh>
2025-07-08 15:18:14 +0700

    exercise5

--- exercise-5.py
+++ exercise-5.py
@@ -1,6 +1,18 @@
 def reverse_ascending(numbers):
-    ...
-    if not isinstance (numbers, (int)):
-        return "invaild input"
+    new_num = []
+    num = []
+    # if not isinstance (numbers, (int)):
+    #     return "invaild input"
+    
+    for i in numbers:
+        if not num or i > num[-1]:
+            num.append(i)
+        else:
+            new_num.extend(reversed(num))
+            num = [i]
+    if num:
+        new_num.extend(reversed(num))
+    return new_num
     
     
+print(reverse_ascending([1, 2, 3, 4, 5]))
