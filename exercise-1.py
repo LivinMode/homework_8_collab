272d24e74c056cec930b4986dfbf937ce96b9793
LivinMode <2024545chhay@aupp.edu.kh>
2025-07-08 15:15:33 +0700

    exercise 1 and 3

--- exercise-1.py
+++ exercise-1.py
@@ -1,2 +1,8 @@
 def replace_last(numbers):
+        # If the list has 0 or 1 numbers, return it as is
+    if len(numbers) <= 1:
+        return numbers
+    # Take the last numbers and add the rest of the list (except last)
+    return [numbers[-1]] + numbers[:-1]
     ...
+print(replace_last([2, 3, 4, 1]))
--- exercise-3.py
+++ exercise-3.py
@@ -1,2 +1,15 @@
 def remove_all_after(numbers, n):
+    # Check if the border number is in the list
+    if n in numbers:
+        # Find the position n in the list
+        position = numbers.index(n)
+        # return number befor n and include n as well
+        result = numbers[:position + 1]
+        return result
+    # If border not found 
+    else:
+        # return the list unchanged
+        return numbers
     ...
+print(remove_all_after([1, 2, 3, 4, 5], 3)) 
+print(remove_all_after([1, 2, 3, 4, 5], 8)) 
