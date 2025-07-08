317401d63c85ea228d3c965136bd2b4a0cdc67c2
Kheang2005 <2024374lim@aupp.edu.kh>
2025-07-08 12:47:37 +0700

    Update exercise-4.py
    test

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,11 @@
 def chunking_by(numbers, chunck):
-    ...
+    resulte = []
+    for i in range (0,len(numbers), chunck):
+        new_data = numbers[i:i + chunck]
+        resulte.append(new_data)
+
+
+    return resulte
+
+
+print(chunking_by([2, 3, 4, 5, 6, 6, 8, 7, 9], 3))
